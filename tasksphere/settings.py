c20b06fd9d9d291c0ac6a72bc4111ce46e9dd94e
NataliaVozhdaeva <lomznat@gmail.com>
2026-04-08 14:57:29 +1200

    fix: cors sredentials

--- tasksphere/settings.py
+++ tasksphere/settings.py
@@ -150,4 +150,5 @@ CORS_ALLOWED_ORIGINS = os.getenv(
     'CORS_ALLOWED_ORIGINS',
     'http://localhost:3000,http://127.0.0.1:3000'
 ).split(',')
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
